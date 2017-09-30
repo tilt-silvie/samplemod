c7a5d2258d8bfe72d636b1dbdc0c00cdf909e534
Makito Ishikura <tilt.silvie@gmail.com>
2017-09-30 23:01:17 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
-    version='0.0.0',
-    description='Sample package for Python-Guide.org',
+    name='NEED_MODIFY',
+    version='0.0.1',
+    description='NEED_MODIFY',
     long_description=readme,
     author='Makito Ishikura',
     author_email='tilt.silvie@gmail.com',
-    url='https://github.com/kennethreitz/samplemod',
+    url='NEED_MODIFY',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
